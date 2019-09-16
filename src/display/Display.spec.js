1ad5c9e4bb165d2d4f882e8d72652b9f88612fb6
RichardEsquivel <rlesquivel@gmail.com>
2019-09-16 17:57:54 -0500

    Got first basic test to run successfully in Display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from 'react';
+import Display from './Display.js';
+import { render } from 'react-testing-library';
+
+
+describe('Display/>', () => {
+	test('renders without crashing', () => {
+		render(<Display />);
+	});
+});
