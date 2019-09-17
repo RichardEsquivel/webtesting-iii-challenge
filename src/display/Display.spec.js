686253a9061fded826e37e94d965263c040c9a9e
RichardEsquivel <rlesquivel@gmail.com>
2019-09-17 07:36:56 -0500

    Basic testing of display component is complete checks initial render of Display and also checks initial loading state of open and unlocked along with locked and closed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Display from './Display.js';
 import { render } from 'react-testing-library';
 
-
+//basic check that Display is rendering
 describe('Display/>', () => {
 	test('renders without crashing', () => {
 		render(<Display />);
+	});
+	//testing initial state of open and unlocked
+	test('open and unlocked', () => {
+		const { getByText } = render(<Display />);
+		getByText(/unlocked/i);
+		getByText(/open/i);
+	});
+	//testing that state props displaying closed and locked are displaying properly
+	test('open and unlocked', () => {
+		const { getByText } = render(<Display closed={true} locked={true} />);
+		//check that unlocked and open text are being displayed not case sensitive in getByText query
+		getByText(/locked/i);
+		getByText(/closed/i);
 	});
 });
